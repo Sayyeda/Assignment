3a52804c471a7520bdf91a5019f321bd08d3fd93
Sayyeda <sayyedaareeba9@gmail.com>
2014-06-09 14:57:46 +1000

    Hashtable

---
+++ Hashtable/Program/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Program
+{
+    partial class HashTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.List = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // List
+            // 
+            this.List.Location = new System.Drawing.Point(133, 47);
+            this.List.Name = "List";
+            this.List.Size = new System.Drawing.Size(75, 23);
+            this.List.TabIndex = 0;
+            this.List.Text = "Add";
+            this.List.UseVisualStyleBackColor = true;
+            this.List.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(133, 108);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(52, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
+            // 
+            // HashTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.List);
+            this.Name = "HashTable";
+            this.Text = "HashTable";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button List;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Hashtable/Program/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Program
+{
+    public partial class HashTable : Form
+    {
+        public HashTable()
+        {
+            InitializeComponent();
+        }
+        
+        Hashtable students = new Hashtable();
+
+        char a;
+        
+        private void Add_Click(object sender, EventArgs e)
+        {         
+            students.Add(textBox2.Text,a);
+            Console.WriteLine("The Hashtable initially contains the following:",textBox2);
+            PrintKeysAndValues(students);    
+        }
+
+        public static void PrintKeysAndValues(Hashtable students)
+        {
+            foreach (DictionaryEntry de in students)
+            Console.WriteLine("    {0}:    {1}", de.Key, de.Value);
+            Console.WriteLine();
+        }
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            students.Remove(textBox1.Text);
+            Console.WriteLine("After removing :");
+            PrintKeysAndValues(students);
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+    }
+}
